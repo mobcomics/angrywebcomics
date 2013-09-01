55954993c2fa7a10e3dab42ebd29c4c74e3e9d41
Tomi Huttula <tomi.huttula@gmail.com>
2013-08-31 17:40:43 -0700

    first commit

---
+++ www/javascript/index.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+window.onload = init;
+
+function init() {
+	document.getElementById("description").innerHTML = editor.description;
+	document.getElementById("post").innerHTML = editor.post;
+//	alert(comics.comicsList[0].comicDataUrl);	
+	}
+	
+function test() {
+	alert("parent");
+	}
